7c22afbd021b0012e601b9f0d179a24e933fb877
Jason <benselj@mail.gvsu.edu>
2016-11-01 17:22:10 +0000

    points new clients to /client/index.html

--- server.js
+++ server.js
@@ -1,10 +1,19 @@
+/**
+ * Initializes app to be a function handler that you can supply to an HTTP server
+ * */
 var app = require('express')();
 var http = require('http').Server(app);
 
+/**
+ * We define a route handler / that gets called when clients hit website home
+ * */
 app.get('/', function(req, res){
-  res.send('<h1>Hello World<h1>');
+  res.sendfile('/client/index.html');
 });
 
+/**
+ * Make the http server listen on port 8080
+ * */
 http.listen(8080, function(){
   console.log('listening on : 8080');
 });
