4e88d71adc2b23c861e57f59a841728d4a6e1b3d
Jason <benselj@mail.gvsu.edu>
2016-11-03 20:45:15 +0000

    Working socket connection between client and server

--- server.js
+++ server.js
@@ -1,21 +1,42 @@
-/**
- * Initializes app to be a function handler that you can supply to an HTTP server
- * */
+
 var express = require('express');
 var app = express();
-var http = require('http').Server(app);
+var server = require('http').Server(app);
+var io = require('socket.io').listen(server);
+
+var path = require('path');
+var async = require('async');
+
+//Define static content for app to use.
+app.use(express.static('client'));
 
 /**
  * We define a route handler / that gets called when clients hit website home
+ * will automatically search for and serve index from this directory.
  * */
-app.get('/', function(req, res){
-  //res.send('<h1>HelloWorld<h1>');
-  res.sendfile('client/index.html')
+app.get('/', function(request, response){
+  response.sendfile(__dirname + '/client')
+});
+
+server.listen(8080, function(){
+  console.log('listening on : 8080');
 });
 
 /**
- * Make the http server listen on port 8080
+ * Implement naming convention for connect and disconnect of each user
  * */
-http.listen(8080, function(){
-  console.log('listening on : 8080');
+io.on('connection', function(socket){
+  console.log('a user has connected');
+  socket.on('disconnect', function(){
+    console.log('a user has disconnected');
+    });
 });
+
+/**
+ * Listen for incoming messages from clients
+ * */
+ io.on('connection', function(socket){
+   socket.on('message to server', function(message){
+     console.log('message from client: ' + message);
+   });
+ });
