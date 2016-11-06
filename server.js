b0601492499680b6901388779c53618138ca8116
Jason <benselj@mail.gvsu.edu>
2016-11-06 02:36:52 +0000

    supports multiple clients, broadcast chat functionality, whisper chat functionality

--- server.js
+++ server.js
@@ -1,4 +1,3 @@
-
 var express = require('express');
 var app = express();
 var server = require('http').Server(app);
@@ -10,33 +9,122 @@ var async = require('async');
 //Define static content for app to use.
 app.use(express.static('client'));
 
+
+var clients = [];
+var count = 0;
+
 /**
  * We define a route handler / that gets called when clients hit website home
  * will automatically search for and serve index from this directory.
  * */
-app.get('/', function(request, response){
-  response.sendfile(__dirname + '/client')
+app.get('/', function(request, response) {
+    response.sendfile(__dirname + '/client')
 });
 
-server.listen(8080, function(){
-  console.log('listening on : 8080');
+server.listen(8080, function() {
+    console.log('listening on : 8080');
 });
 
 /**
- * Implement naming convention for connect and disconnect of each user
+ * 
+ * Creates a client object after successfull socket creation
+ * broadcasts to all users that a new user has connected
+ * saves client information for user list
+ * 
+ * TODO: Remove disconnected clients from client list
+ * 
  * */
-io.on('connection', function(socket){
-  console.log('a user has connected');
-  socket.on('disconnect', function(){
-    console.log('a user has disconnected');
+io.on('connection', function(socket) {
+
+    socket.on('client name', function(name) {
+
+        var client = new Object();
+
+        if (name === "") {
+            name = 'anonymous' + count;
+            client.name = name;
+            count++;
+        }
+        else {
+            client.name = name;
+        }
+
+        client.socket = socket;
+        
+        clients.push(client);
+
+        console.log('Client name: ' + client.name + '\nID: ' + client.socket);
+        
+        io.sockets.emit('broadcast', 'user connected');
+        
+        updateUserList();
+    });
+
+    socket.on('disconnect', function() {
+        console.log(socket.id + "client disconnected");
+        for(var i = 0; i < clients.length; i++)
+        {
+            if(clients[i].socket.id === socket.id)
+            {
+                clients.splice(i, 1);
+                
+            }
+            
+            updateUserList();
+        }
+        
+
+    });
+    
+    
+    /**
+     * Listens for incoming message from clients, broadcasts message to all clients
+     * 
+     * TODO: add whisper functionality
+    * */
+    
+    socket.on('message to server', function(message) {
+        
+        //Break apart message and check if its a whisper
+        var messageArr = message.split(" ", 2);
+        
+        if(messageArr[0] === '/w'){
+            //check clients list for name
+            for(var pos in clients){
+                if(clients[pos].name == messageArr[1]){
+                    console.log(clients[pos].socket.id);
+                    
+                    io.sockets.socket(clients[pos].socket.id).emit('broadcast', 'super secret message');
+
+                    //io.connected[clients[pos].socket.id].emit();
+                    
+                }
+            }
+        }
+        else{
+            io.sockets.emit('broadcast', message);
+        }
+        console.log('message from client: ' + message);        
     });
 });
 
+
+
+
 /**
- * Listen for incoming messages from clients
+ * Iterates through clients and provides an comma delimited list which is 
+ * broadcasted to each client for an up to date list of users
  * */
- io.on('connection', function(socket){
-   socket.on('message to server', function(message){
-     console.log('message from client: ' + message);
-   });
- });
+function updateUserList() {
+    var clientList = "";
+    for (var i = 0; i < clients.length; i++) 
+    {
+        if (i === 0) 
+        {
+            clientList = clients[i].name;
+        }
+
+        clientList = clientList + ',' + clients[i].name;
+    }
+    io.sockets.emit('updated user list', clientList);
+}
